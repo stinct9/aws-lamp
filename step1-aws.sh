b985b5405e3ed094b9a5241fd131ad90b6626c06
J <j@J.local>
2017-02-01 12:13:26 +0530

    Added script for basic setup of server

---
+++ step1-aws.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Instructions to use this script 
+#
+#chmod +x SCRIPTNAME.sh
+#
+#sudo ./SCRIPTNAME.sh
+
+echo "###################################################################################"
+echo "Please be Patient: Installation will start now.......and it will take some time :)"
+echo "###################################################################################"
+
+sudo yum update -y
+
+sudo yum install -y httpd24 php56 mysql55-server php56-mysqlnd
+
+sudo service httpd start
+
+sudo chkconfig httpd on
+
+chkconfig --list httpd
+
+sudo groupadd www
+
+sudo usermod -a -G www ec2-user
+
+exit
