971e91076d047b3b8c3785b3709bfbfd5789fe14
Jay <jay2689@gmail.com>
2018-10-11 17:35:19 +0530

    test

--- 3-aws.sh
+++ 3-aws.sh
@@ -1,39 +1,36 @@
-#!/bin/bash
-
-#Instructions to use this script 
-#
-#chmod +x SCRIPTNAME.sh
-#
-#sudo ./SCRIPTNAME.sh
-
-echo "###################################################################################"
-echo "Step 4 Installation will start now......."
-echo "###################################################################################"
-
-rm /var/www/html/phpinfo.php
-
-sudo service mysqld start
-
-sudo mysql_secure_installation
-
-sudo chkconfig mysqld on
-
-
-sudo yum-config-manager --enable epel
-
-sudo yum install -y phpMyAdmin
-
-sudo sed -i -e 's/127.0.0.1/49.33.67.39/g' /etc/httpd/conf.d/phpMyAdmin.conf
-
-sudo service httpd restart
-
-sudo service mysqld restart
-
-
-
-
-echo "###################################################################################"
-echo "Exiting, please login again"
-echo "###################################################################################"
-
-exit
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              71.71875,
+              14.093957177836224
+            ],
+            [
+              86.923828125,
+              14.093957177836224
+            ],
+            [
+              86.923828125,
+              25.799891182088334
+            ],
+            [
+              71.71875,
+              25.799891182088334
+            ],
+            [
+              71.71875,
+              14.093957177836224
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}
